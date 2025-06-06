4db514b80c2bc42a32db38a3098450e4549fa512
Cyrille DE GASQUET <pro.cyrille.degasquet@gmail.com>
2025-06-06 05:34:38 +0200

    Débuggage css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
-import './globals.css'
-import '../../public/stylesheets/application.css'
 import Navbar from './components/Navbar'
+import { Providers } from './providers'
 
 export const metadata = {
 	title: 'Magasin de Plantes'
@@ -13,38 +12,19 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 				<meta charSet="UTF-8" />
 				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 				<title>Magasin de Plantes</title>
-				{/* Icône du site */}
 				<link rel="icon" href="/favicon.ico" type="image/x-icon" />
-				{/* Bootstrap CSS preload pour optimisation */}
-				<link
-					rel="preload"
-					href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
-					as="style"
-					onLoad="this.rel='stylesheet'"
-				/>
-				{/* Fallback si JavaScript désactivé */}
-				<noscript>
-					<link
-						rel="stylesheet"
-						href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
-					/>
-				</noscript>
+				<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" />
+				<link rel="stylesheet" href="/stylesheets/application.css" />
 			</head>
 			<body>
-				{/* Barre de navigation */}
-				<Navbar />
-				{/* Container principal */}
-				<div className="container mt-4">
-					{/* TODO: Flash messages React component */}
-					{children}
-				</div>
-				{/* Bootstrap JS bundle */}
-				<script
-					src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-					defer
-				/>
-				{/* Application JS hérité de Rails */}
-				<script src="/javascripts/application.js" defer />
+				<Providers>
+					<Navbar />
+					<div className="container mt-4">
+						{children}
+					</div>
+				</Providers>
+				<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" />
+				<script src="/javascripts/application.js" />
 			</body>
 		</html>
 	)
